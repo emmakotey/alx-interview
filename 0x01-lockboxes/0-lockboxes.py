5a9880bc5c16289243f5e3cceb206a75b613650e
emmakotey <emmakotey@gmail.com>
2023-07-05 07:19:07 +0000

    load

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""Defines a function that determines if a box containing a list
-   of lists can be opened using keys stored in the lists
+"""This function accertains if a box containing a list
+   can be opened using keys stored in those lists
 """
 
 
