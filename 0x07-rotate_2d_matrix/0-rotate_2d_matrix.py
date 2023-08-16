251fa001fe94b5a8e4766af87725f5a0600f6529
emmakotey <emmakotey@gmail.com>
2023-08-16 07:05:29 +0000

    one

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -2,6 +2,8 @@
 """
 Test 0x07 - Rotate 2D Matrix
 """
+
+
 def rotate_2d_matrix(matrix):
     """
     Rotates a 2D matrix 90 degrees clockwise.
@@ -16,6 +18,8 @@ def rotate_2d_matrix(matrix):
             matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j]
             matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i]
             matrix[j][n - 1 - i] = temp
+
+
 if __name__ == "__main__":
     matrix = [[1, 2, 3],
               [4, 5, 6],
