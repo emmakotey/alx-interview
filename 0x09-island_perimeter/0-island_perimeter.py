197aee44e12171809e1b50e2a65d8eb4c9b18bd0
emmakotey <emmakotey@gmail.com>
2023-08-30 07:31:27 +0000

    on

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -51,14 +51,8 @@ def boundary(grid, i, j):
 
 def island_perimeter(grid):
     """
-    Calculate and return perimeter of island in the grid
-    Grid is a rectangular grid where 0s represent water and 1s represent land
-    Each cell is a square with a side length of 1
-    There is only one island
-    Args:
-        grid [list] : 2d list of ints either 0 or 1
-    Return:
-       perimeter of island
+    This section checks for conditions and returns the correct
+    or expected output.
     """
     if grid == []:
         return 0
